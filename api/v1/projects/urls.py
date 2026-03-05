cdd5a76279ab126251dd39f2515a70ded4a2d9eb
Anastasiia Koriahina <anastasia1koryagina@gmail.com>
2026-03-05 17:47:35 +0100

    feat: add delete project api

--- api/v1/projects/urls.py
+++ api/v1/projects/urls.py
@@ -4,4 +4,9 @@ from api.v1.projects import views
 
 urlpatterns = [
     path("", views.TravelProjectListApi.as_view(), name="project-list"),
+    path(
+        "delete/<int:pk>/",
+        views.TravelProjectDeleteAPI.as_view(),
+        name="project-delete",
+    ),
 ]
--- api/v1/projects/views.py
+++ api/v1/projects/views.py
@@ -1,5 +1,8 @@
 from rest_framework import status
-from rest_framework.generics import ListAPIView
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListAPIView, GenericAPIView
+from rest_framework.response import Response
+from rest_framework.request import Request
 from api.v1.projects.serializers import TravelProjectDisplaySerializer
 from projects.models import TravelProject
 
@@ -7,3 +10,20 @@ from projects.models import TravelProject
 class TravelProjectListApi(ListAPIView):
     queryset = TravelProject.objects.all()
     serializer_class = TravelProjectDisplaySerializer
+
+
+class TravelProjectDeleteAPI(GenericAPIView):
+    def delete(self, request: Request, pk: int):
+        project = get_object_or_404(TravelProject, pk=pk)
+
+        if project.places.filter(is_visited=True).exists():
+            return Response(
+                {
+                    "error": "This project cannot be deleted because it contains visited places."
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        project.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
