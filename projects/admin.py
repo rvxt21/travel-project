b3ce51f4093acc56e3b1c01a1f8c511e59d73715
Anastasiia Koriahina <anastasia1koryagina@gmail.com>
2026-03-05 15:50:28 +0100

    feat: implement inline admin for project places

--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from projects.models import TravelProject
+from places.models import Place
+
+
+class TravelPlaceTabular(admin.TabularInline):
+    model = Place
+    extra = 0
 
 
 class TravelProjectAdmin(admin.ModelAdmin):
+    inlines = [
+        TravelPlaceTabular,
+    ]
     list_display = ["name", "description", "start_date", "status"]
 
 
